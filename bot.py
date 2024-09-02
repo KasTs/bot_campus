617fceaa61c441b70efbdfe5ea53301677b88452
Thiago Fagundes <thiago.fagundes@cilia.com.br>
2024-09-02 15:11:19 -0300

    Remove telegram key

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from llama_index.core import StorageContext, load_index_from_storage
 
 
 #deletar linha de token posteriormente.
-bot = telebot.TeleBot('6848456240:AAF9PvYwRWxYnwl8GdRkqDeBxQ5Hr0_-Mo8')
+# bot = telebot.TeleBot('6848456240:AAF9PvYwRWxYnwl8GdRkqDeBxQ5Hr0_-Mo8')
 
 documents = SimpleDirectoryReader('./dados').load_data()
 
